4d7b8e3dbe8d71b85057d9976d41c522e48c3359
StevenPapalia <papaliasteven@gmail.com>
2020-03-02 11:27:17 -0800

    Set up board, made a function that retrieves innerHTML and ID

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,44 @@
+var squares = document.getElementsByClassName('square');
 
+// var rowOne =[];
+// var rowTwo = [];
+// var rowThree = [];
+// for (var i = 0; i < squares.length; i++) {
+//   if (i < 3) {
+//     rowOne.push(squares[i].innerHTML);
+//   } else if (i < 6) {
+//     rowTwo.push(squares[i].innerHTML);
+//   } else {
+//     rowThree.push(squares[i].innerHTML);
+//   }
+// }
+// var board = [rowOne, rowTwo, rowThree];
+// console.log(board);
+
+var board = [];
+for (var i = 0; i < squares.length; i++) {
+    board.push(squares[i].innerHTML);
+}
+console.log(board);
+
+var xCount = 0;
+
+var makeMove = function(htmlContents , id) {
+    // if square not already clicked
+    if (htmlContents) {
+      console.log('valid move');
+      // if number of x % 2 === 0
+      if (xCount % 2 === 0) {
+        // add an X where clicked
+        console.log(htmlContents, id);
+      }
+      // else
+        // add a O where clicked
+    } else {
+      console.log('please play a move on an available sqaure!');
+    }
+
+
+  // update board
+};
 
