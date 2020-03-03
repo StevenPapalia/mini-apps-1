3765aa52f15dee5c3c3c1a0c0603c0521d57efc4
StevenPapalia <papaliasteven@gmail.com>
2020-03-03 12:26:42 -0800

    got XHR sent, cannot find value that I sent it on server

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,38 @@
+document.getElementById("button").onclick = function sendJSONandGetCSV() {
+  // grab data
+  var text = document.getElementById("textfield").value;
 
+  // send data to server
+  var xhr = new XMLHttpRequest();
+
+  xhr.open("POST", '../converter', true);
+
+  xhr.send({ text: text });
+
+  xhr.onload = function() {
+    if (xhr.status !== 200) { // analyze HTTP status of the response
+      alert(`Error ${xhr.status}: ${xhr.statusText}`); // e.g. 404: Not Found
+    } else { // show the result
+      alert(`Done, got ${xhr.response.length}`); // responseText is the server
+    }
+  };
+
+  xhr.onprogress = function(event) {
+    if (event.lengthComputable) {
+      console.log(`Received ${event.loaded} of ${event.total} bytes`);
+    } else {
+      console.log(`Received ${event.loaded} bytes`); // no Content-Length
+    }
+
+  };
+
+  xhr.onerror = function() {
+    console.log("Request failed");
+  };
+
+  xhr.timeout = 10000; // timeout in ms, 10 seconds
+
+
+
+};
 
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -2,7 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('hello'));
+
+app.post('/converter', (req, res) => {
+  console.log(req.body);
+  res.send('hello client');
+});
 
 app.listen(port, () => console.log(`I am listening on ${port}, beware...`));
 
+app.use('/', express.static('client'));
