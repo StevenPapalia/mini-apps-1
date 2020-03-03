6c293c917bf36133476af82816175fbccbd8b9a9
StevenPapalia <papaliasteven@gmail.com>
2020-03-03 10:22:09 -0800

    Installed dependencies, got express server running using nodemon

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => res.send('hello'));
+
+app.listen(port, () => console.log(`I am listening on ${port}, beware...`));
 
